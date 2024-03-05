d0694f32e98fea768947df42136219299389b720
Ahwx <ahwx@ahwx.org>
2024-03-06 00:27:01 +0100

    feat(keymappings.nix): adds keybindings for plugins that require keybindings

---
+++ config/keymappings.nix
@@ -0,0 +1,86 @@
+{self, ...}: {
+  keymaps = [
+
+    # Neo-tree bindings
+    {
+      action = "<cmd>Neotree toggle<CR>";
+      key = "<leader>e";
+    }
+
+    # Telescope bindings
+
+    {
+      action = "<cmd>Telescope live_grep<CR>";
+      key = "<leader>fw";
+    }
+    {
+      action = "<cmd>Telescope find_files<CR>";
+      key = "<leader>ff";
+    }
+    {
+      action = "<cmd>Telescope git_commits<CR>";
+      key = "<leader>fg";
+    }
+
+    # Notify bindings
+
+    {
+      mode = "n";
+      key = "<leader>un";
+      action = ''
+        <cmd>lua require("notify").dismiss({ silent = true, pending = true })<cr>
+      '';
+      options = {
+        desc = "Dismiss All Notifications";
+      };
+    }
+
+
+    # Bufferline bindings
+
+    {
+      mode = "n";
+      key = "<Tab>";
+      action = "<cmd>BufferLineCycleNext<cr>";
+      options = {
+        desc = "Cycle to next buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<S-Tab>";
+      action = "<cmd>BufferLineCyclePrev<cr>";
+      options = {
+        desc = "Cycle to previous buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<S-l>";
+      action = "<cmd>BufferLineCycleNext<cr>";
+      options = {
+        desc = "Cycle to next buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<S-h>";
+      action = "<cmd>BufferLineCyclePrev<cr>";
+      options = {
+        desc = "Cycle to previous buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bd";
+      action = "<cmd>bdelete<cr>";
+      options = {
+        desc = "Delete buffer";
+      };
+    }
+  ];
+}
