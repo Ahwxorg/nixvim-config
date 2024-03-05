44a5b434de87edda973e80492a14b63f1e27f266
Ahwx <ahwx@ahwx.org>
2024-03-06 00:16:18 +0100

    feat(plugins.nix): adds colorscheme and options

--- config/plugins.nix
+++ config/plugins.nix
@@ -1,1 +1,36 @@
-
+{pkgs, ...}: {
+  colorschemes.catppuccin = {
+    enable = true;
+    disableBold = false;
+    disableItalic = false;
+    disableUnderline = false;
+    transparentBackground = false;
+    integrations = {
+      cmp = true;
+      noice = true;
+      notify = true;
+      neotree = true;
+      harpoon = true;
+      gitsigns = true;
+      which_key = true;
+      illuminate.enabled = true;
+      treesitter = true;
+      treesitter_context = true;
+      telescope.enabled = true;
+      indent_blankline.enabled = true;
+      mini.enabled = true;
+      native_lsp = {
+        enabled = true;
+        inlay_hints = {
+          background = true;
+        };
+        underlines = {
+          errors = ["underline"];
+          hints = ["underline"];
+          information = ["underline"];
+          warnings = ["underline"];
+        };
+      };
+    };
+  };
+}
