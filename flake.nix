c26d68c39ebe8ef6ac01e9ab899399d1a1aeab0c
Ahwx <ahwx@ahwx.org>
2024-03-06 00:04:36 +0100

    feat(flake.nix): adds description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A nixvim configuration";
+  description = "Ahwx' Neovim configuration, declaratively written using Nix";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
