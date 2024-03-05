bc18ddcea33ef90ed75c2a2ed9c482ad770fbaa7
Ahwx <ahwx@ahwx.org>
2024-03-06 00:33:56 +0100

    feat(options.nix): set some options

--- config/options.nix
+++ config/options.nix
@@ -1,1 +1,26 @@
+{self, ...}: {
 
+  # options = {
+  #   number = true;
+  #   relativenumber = true;
+
+  #   shiftwidth = 2;
+  # };
+
+  globals.mapleader = " ";
+
+  # autoCmd = [
+  #   {
+  #     event = [ "BufEnter" "BufWinEnter" ];
+  #     pattern = [ "*.c" "*.h" ];
+  #     command = [ "echo 'Entering a C or C++ file" ];
+  #   }
+  # ];
+
+  highlight = {
+    Comment.fg = "#ff00ff";
+    Comment.bg = "#000000";
+    Comment.underline = true;
+    Comment.bold = true;
+  };
+}
