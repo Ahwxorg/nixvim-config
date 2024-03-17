3ad6a9d7214a576211fdf826ef43ead939e5482e
Ahwx <ahwx@ahwx.org>
2024-03-17 22:55:15 +0100

    revert: temporary disable none-ls due to issues with nix

--- config/plugins.nix
+++ config/plugins.nix
@@ -30,47 +30,47 @@
       enable = true;
     };
 
-    none-ls = {
-      enable = true;
-      cmd = ["bash -c nvim"];
-      debug = true;
-      sources = {
-        code_actions = {
-          statix.enable = true;
-          gitsigns.enable = true;
-        };
-        diagnostics = {
-          statix.enable = true;
-          deadnix.enable = true;
-          pylint.enable = true;
-          checkstyle.enable = true;
-        };
-        formatting = {
-          alejandra.enable = true;
-          stylua.enable = true;
-          shfmt.enable = true;
-          nixpkgs_fmt.enable = true;
-          google_java_format.enable = false;
-          prettier = {
-            enable = true;
-            disableTsServerFormatter = true;
-          };
-          black = {
-            enable = true;
-            withArgs = ''
-              {
-                extra_args = { "--fast" },
-              }
-            '';
+    #  none-ls = {
+    #    enable = true;
+    #    cmd = ["bash -c nvim"];
+    #    debug = true;
+    #    sources = {
+    #      code_actions = {
+    #        statix.enable = true;
+    #        gitsigns.enable = true;
+    #      };
+    #      diagnostics = {
+    #        statix.enable = true;
+    #        deadnix.enable = true;
+    #        pylint.enable = true;
+    #        checkstyle.enable = true;
+    #      };
+    #      formatting = {
+    #        alejandra.enable = true;
+    #        stylua.enable = true;
+    #        shfmt.enable = true;
+    #        nixpkgs_fmt.enable = true;
+    #        google_java_format.enable = false;
+    #        prettier = {
+    #          enable = true;
+    #          disableTsServerFormatter = true;
+    #        };
+    #        black = {
+    #          enable = true;
+    #          withArgs = ''
+    #            {
+    #              extra_args = { "--fast" },
+    #            }
+    #          '';
 
-          };
-        };
-        completion = {
-          luasnip.enable = true;
-          spell.enable = true;
-        };
-      };
-    };
+    #        };
+    #      };
+    #      completion = {
+    #        luasnip.enable = true;
+    #        spell.enable = true;
+    #      };
+    #    };
+    #  };
 
     # Notify
     notify = {
