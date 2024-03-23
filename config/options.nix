15cd4e005cb26ac035ae551fbdf466dbb48f3a7a
Ahwx <ahwx@ahwx.org>
2024-03-23 12:16:29 +0100

    feat: add some more options

--- config/options.nix
+++ config/options.nix
@@ -1,9 +1,36 @@
 {self, ...}: {
   globalOptions = {
+    # Line numbers
     number = true;
     relativenumber = true;
 
-    shiftwidth = 2;
+    # Always show the signcolumn, otherwise text would be shifted when displaying error icons
+    signcolumn = "yes";
+
+    # Search
+    ignorecase = true;
+    smartcase = true;
+
+    # Tab defaults (might get overwritten by an LSP server)
+    tabstop = 4;
+    shiftwidth = 4;
+    softtabstop = 0;
+    expandtab = true;
+    smarttab = true;
+
+    # System clipboard support, needs xclip/wl-clipboard
+    #clipboard = "unnamedplus";
+
+    # Highlight the current line
+    cursorline = true;
+
+    # Show line and column when searching
+    ruler = true;
+
+    # Global substitution by default
+    gdefault = true;
+
+    # Start scrolling when the cursor is X lines away from the top/bottom
     scrolloff = 5;
   };
 
