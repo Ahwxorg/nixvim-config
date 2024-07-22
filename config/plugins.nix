75de21b1df8d7759d0d01e29f0b7e03e60ebc1c9
Ahwx <ahwx@ahwx.org>
2024-07-22 13:32:39 +0200

    feat: adds markview

--- config/plugins.nix
+++ config/plugins.nix
@@ -629,6 +629,16 @@
       clipboard-image-nvim
     ]
     ++ [
+      (pkgs.vimUtils.buildVimPlugin {
+      pname = "markview.nvim";
+      version = "0.0.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "OXY2DEV";
+        repo = "markview.nvim";
+        rev = "a959d77ca7e9f05175e3ee4e582db40b338c9164";
+        hash = "sha256-w6yn8aNcJMLRbzaRuj3gj4x2J/20wUROLM6j39wpZek=";
+      };
+    })
       # (pkgs.vimUtils.buildVimPlugin {
       #   pname = "accelerated-jk";
       #   src = pkgs.fetchFromGitHub {
