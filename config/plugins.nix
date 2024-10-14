50799753e4ee397c847018ee4f3af1daaf882056
Ahwx <ahwx@ahwx.org>
2024-10-14 13:57:57 +0200

    feat: adds image so that images render in markdown

--- config/plugins.nix
+++ config/plugins.nix
@@ -353,6 +353,31 @@
       };
     };
 
+    image = {
+      enable = true;
+      backend = "kitty";
+      hijackFilePatterns = [
+        "*.png"
+        "*.jpg"
+        "*.jpeg"
+        "*.gif"
+        "*.webp"
+      ];
+      maxHeightWindowPercentage = 25;
+      tmuxShowOnlyInActiveWindow = true;
+      integrations = {
+        markdown = {
+          enabled = true;
+          downloadRemoteImages = true;
+          filetypes = [
+            "markdown"
+            "vimwiki"
+            "mdx"
+          ];
+        };
+      };
+    };
+
     # Prettier fancier command window
     noice = {
       enable = true;
