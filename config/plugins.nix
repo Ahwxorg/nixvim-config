c8886dbdd47886ab4c188d6ff243c3d8798073e3
Ahwx <ahwx@ahwx.org>
2024-11-07 13:24:34 +0100

    fix: markdown rendering

--- config/plugins.nix
+++ config/plugins.nix
@@ -922,30 +922,29 @@
   extraPlugins = with pkgs.vimPlugins;
     [
       vim-be-good
-      headlines-nvim # Should load this in at the opening of filetypes that require this, namely Markdown.
       glow-nvim # Glow inside of Neovim
       clipboard-image-nvim
-    ]
-    ++ [
-      (pkgs.vimUtils.buildVimPlugin {
-      pname = "markview.nvim";
-      version = "0.0.1";
-      src = pkgs.fetchFromGitHub {
-        owner = "OXY2DEV";
-        repo = "markview.nvim";
-        rev = "a959d77ca7e9f05175e3ee4e582db40b338c9164";
-        hash = "sha256-w6yn8aNcJMLRbzaRuj3gj4x2J/20wUROLM6j39wpZek=";
-      };
-    })
-      # Just copy this block for a new plugin
-      # (pkgs.vimUtils.buildVimPlugin {
-      #   pname = "";
-      #   src = pkgs.fetchFromGitHub {
-      #     owner = "";
-      #     repo = "";
-      #     rev = "";
-      #     sha256 = "";
-      #   };
-      # })
     ];
+    #    ++ [
+    #      (pkgs.vimUtils.buildVimPlugin {
+    #      pname = "markview.nvim";
+    #      version = "0.0.1";
+    #      src = pkgs.fetchFromGitHub {
+    #        owner = "OXY2DEV";
+    #        repo = "markview.nvim";
+    #        rev = "a959d77ca7e9f05175e3ee4e582db40b338c9164";
+    #        hash = "sha256-w6yn8aNcJMLRbzaRuj3gj4x2J/20wUROLM6j39wpZek=";
+    #      };
+    #    })
+    #      # Just copy this block for a new plugin
+    #      # (pkgs.vimUtils.buildVimPlugin {
+    #      #   pname = "";
+    #      #   src = pkgs.fetchFromGitHub {
+    #      #     owner = "";
+    #      #     repo = "";
+    #      #     rev = "";
+    #      #     sha256 = "";
+    #      #   };
+    #      # })
+    #    ];
 }
