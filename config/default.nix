c56399ff3e5fb191935365726b22e2416a06e48d
Ahwx <ahwx@ahwx.org>
2025-03-31 17:02:44 +0200

    feat: import new organised modules

--- config/default.nix
+++ config/default.nix
@@ -1,8 +1,30 @@
 {
   # Import all your configuration modules here
   imports = [
-    ./plugins.nix
+    # ./plugins.nix
     ./options.nix
     ./keymappings.nix
+    ./modules/plugins/autopairs.nix
+    ./modules/plugins/blink.nix
+    ./modules/plugins/bufferline.nix
+    ./modules/plugins/cmp.nix
+    ./modules/plugins/colorscheme.nix
+    ./modules/plugins/comment.nix
+    ./modules/plugins/comment-box.nix
+    ./modules/plugins/conform.nix
+    ./modules/plugins/gitsigns.nix
+    ./modules/plugins/icons.nix
+    ./modules/plugins/lint.nix
+    ./modules/plugins/lsp.nix
+    ./modules/plugins/lualine.nix
+    ./modules/plugins/lz-n.nix
+    ./modules/plugins/markview.nix
+    ./modules/plugins/noice.nix
+    ./modules/plugins/none-ls.nix
+    ./modules/plugins/schemastore.nix
+    ./modules/plugins/telescope.nix
+    ./modules/plugins/treesitter.nix
+    ./modules/plugins/trouble.nix
+    ./modules/plugins/trunk.nix
   ];
 }
