2f7012d2515ccfc9e54bf137f02e79d68c1a0e03
Ahwx <ahwx@ahwx.org>
2025-04-17 16:00:34 +0200

    feat: adds neotree

---
+++ config/modules/plugins/neotree.nix
@@ -0,0 +1,25 @@
+{
+  neo-tree = {
+    enable = true;
+    enableDiagnostics = true;
+    enableGitStatus = true;
+    enableModifiedMarkers = true;
+    enableRefreshOnWrite = true;
+    closeIfLastWindow = true;
+    popupBorderStyle = "rounded"; # Type: null or one of “NC”, “double”, “none”, “rounded”, “shadow”, “single”, “solid” or raw lua code
+    buffers = {
+      bindToCwd = false;
+      followCurrentFile = {
+        enabled = true;
+      };
+    };
+    window = {
+      width = 40;
+      height = 15;
+      autoExpandWidth = false;
+      mappings = {
+        "<space>" = "none";
+      };
+    };
+  };
+}
