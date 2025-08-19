76880a7e5930e8255f0d5c19b865beada4bf0573
Ahwx <ahwx@ahwx.org>
2025-08-19 16:07:44 +0200

    chore: adds `nvim-biscuits`

--- config/modules/plugins/others.nix
+++ config/modules/plugins/others.nix
@@ -4,16 +4,17 @@
     vim-be-good
     glow-nvim # Glow inside of Neovim
     clipboard-image-nvim # This might work with emails, not too sure?
-    #]
+    nvim-biscuits
+    # ]
     #++ [
     #  (pkgs.vimUtils.buildVimPlugin {
-    #    pname = "himalaya-vim";
+    #    pname = "nvim-biscuits";
     #    version = "0.0.1";
     #    src = pkgs.fetchFromGitHub {
-    #      owner = "pimalaya";
-    #      repo = "himalaya-vim";
-    #      rev = "";
-    #      hash = "";
+    #      owner = "code-biscuits";
+    #      repo = "nvim-biscuits";
+    #      rev = "ff1d12c8b47cd28723da593b2cfa2e98391d439a";
+    #      sha256 = "18dvvg32nxrdp1ydbvxrzkdg7q214naq2bphs7y1s9zmjhyj25pm";
     #    };
     #  })
     # Just copy this block for a new plugin
